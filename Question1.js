d07903bad8fd8246627c2dc1a7420b10891b4f46
Aye Chan Han <ayechan.han@mo.com.mm>
2023-08-05 17:12:46 +0630

    add question 1

---
+++ Question1.js
@@ -0,0 +1,16 @@
+// Sort the people by name and return as a comma separated string. 
+
+	// $people = array( 'Mary' => 17, 'Bob' => 62, 'John' => 32, ); 
+	// function list_people($people) {
+
+
+function list_people(people) {
+    let name_list = Object.keys(Object.assign({}, ...people))
+    name_list.sort((a, b) => {
+        return a.localeCompare(b)
+    })
+    return name_list
+}
+
+people = [{"Mary": 17}, {"Bob": 62}, {"John": 32}]
+console.log(list_people(people))
